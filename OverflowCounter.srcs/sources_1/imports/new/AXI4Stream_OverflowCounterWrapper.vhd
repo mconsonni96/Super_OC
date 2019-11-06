116095ce893c1fa16012f83a12fdd7730823ff67
Nicola Lusardi <nicola.lusardi@polimi.it>
2019-11-06 17:02:17 +0100

    Add new file to project AXI4Stream_OverflowCounterWrapper

---
+++ OverflowCounter.srcs/sources_1/imports/new/AXI4Stream_OverflowCounterWrapper.vhd
@@ -0,0 +1,175 @@
+------------------------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------------------------------------
+--                                                                                                                     --
+--  __/\\\\\\\\\\\\\\\__/\\\\\\\\\\\\\\\__/\\\\\\\\\\\\_____/\\\\\\\\\\\__/\\\\\\\\\\\\\\\__/\\\_____________          --
+--   _\///////\\\/////__\/\\\///////////__\/\\\////////\\\__\/////\\\///__\/\\\///////////__\/\\\_____________         --
+--    _______\/\\\_______\/\\\_____________\/\\\______\//\\\_____\/\\\_____\/\\\_____________\/\\\_____________        --
+--     _______\/\\\_______\/\\\\\\\\\\\_____\/\\\_______\/\\\_____\/\\\_____\/\\\\\\\\\\\_____\/\\\_____________       --
+--      _______\/\\\_______\/\\\///////______\/\\\_______\/\\\_____\/\\\_____\/\\\///////______\/\\\_____________      --
+--       _______\/\\\_______\/\\\_____________\/\\\_______\/\\\_____\/\\\_____\/\\\_____________\/\\\_____________     --
+--        _______\/\\\_______\/\\\_____________\/\\\_______/\\\______\/\\\_____\/\\\_____________\/\\\_____________	   --
+--         _______\/\\\_______\/\\\\\\\\\\\\\\\_\/\\\\\\\\\\\\/____/\\\\\\\\\\\_\/\\\\\\\\\\\\\\\_\/\\\\\\\\\\\\\\\_   --
+--          _______\///________\///////////////__\////////////_____\///////////__\///////////////__\///////////////__  --
+--                                                                                                                     --
+-------------------------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------------------------------------
+
+------------------------------------ DESCRIPTION ---------------------------------
+----------------------------------------------------------------------------------
+--			Wrapper of Overflow Counter in Timestamp AXI4 Stream				--
+----------------------------------------------------------------------------------
+----------------------------------------------------------------------------------
+
+
+---------- DEFAULT LIBRARY ---------
+library IEEE;
+	use IEEE.STD_LOGIC_1164.all;
+	use IEEE.NUMERIC_STD.ALL;
+	--use IEEE.MATH_REAL.all;
+
+-- library STD;
+	-- 	use STD.textio.all;
+------------------------------------
+
+
+---------- OTHERS LIBRARY ----------
+
+-- library UNISIM;
+-- 	use UNISIM.VComponents.all;
+
+-- library xpm;
+-- 	use xpm.vcomponents.all;
+
+
+-- library work;
+
+------------------------------------
+
+
+
+
+
+entity AXI4Stream_OverflowCounterWrapper is
+
+	generic (
+
+		---------- Calibrated Timestamp Dimension ----
+		BIT_FID				:	NATURAL							:=	1;			        -- Function ID of the Belt Bus 0 = OVERFLOW Coarse, 1 = MEASURE, If BIT_FID = 0 the belt bus is removed and it is a standard axi4 stream
+		BIT_COARSE			:	NATURAL		RANGE	0   TO	32	:=	8;					-- Bit of Coarse Counter, If 0 not Coarse counter is considered only Fine
+		BIT_RESOLUTION      :	POSITIVE	RANGE	1	TO	32	:=	16					-- Number of Bits of the Calibrated_TDL
+		----------------------------------------------
+	);
+
+	port(
+
+		------------------ Reset/Clock ---------------
+		--------- Reset --------
+		reset   : IN    STD_LOGIC;														                                        --  Asynchronous system reset active '1'
+		------------------------
+
+		--------- Clocks -------
+		clk     : IN    STD_LOGIC;			 											                                        -- System clock
+		------------------------
+		----------------------------------------------
+
+		--------------- Timestamp Input ---------------
+		s00_timestamp_tvalid	:	IN	STD_LOGIC;																                -- Valid Timestamp
+		s00_timestamp_tdata		:	IN	STD_LOGIC_VECTOR(BIT_FID + BIT_COARSE + BIT_RESOLUTION-1 DOWNTO 0);   					-- Timestamp dFID + COARSE + RESOLUTION
+		-----------------------------------------------
+
+		--------------- BeltBus Output ----------------
+		m00_beltbus_tvalid	   :	OUT	STD_LOGIC;																                -- Valid Belt Bus
+		m00_beltbus_tdata	   :	OUT	STD_LOGIC_VECTOR(BIT_FID + BIT_COARSE + BIT_RESOLUTION-1 DOWNTO 0) 						-- Belt Bus
+		-----------------------------------------------
+
+	);
+
+end AXI4Stream_OverflowCounterWrapper;
+
+architecture Behavioral of AXI4Stream_OverflowCounterWrapper is
+
+
+	--------------------- Components Declaration ---------------------
+
+	COMPONENT OverflowCounter
+		generic (
+
+			---------- Calibrated Timestamp Dimension ----
+		    BIT_FID				:	NATURAL							:=	1;			        -- Function ID of the Belt Bus 0 = OVERFLOW Coarse, 1 = MEASURE, If BIT_FID = 0 the belt bus is removed and it is a standard axi4 stream
+			BIT_COARSE			:	NATURAL		RANGE	0   TO	32	:=	8;					-- Bit of Coarse Counter, If 0 not Coarse counter is considered only Fine
+			BIT_RESOLUTION      :	POSITIVE	RANGE	1	TO	32	:=	16					-- Number of Bits of the Calibrated_TDL
+			----------------------------------------------
+		);
+
+		port(
+
+			------------------ Reset/Clock ---------------
+			--------- Reset --------
+			reset   : IN    STD_LOGIC;														                        --  Asynchronous system reset active '1'
+			------------------------
+
+			--------- Clocks -------
+			clk     : IN    STD_LOGIC;			 											                        -- System clock
+			------------------------
+			----------------------------------------------
+
+			--------------- Timestamp Input ---------------
+			timestamp_tvalid	:	IN	STD_LOGIC;															        -- Valid Timestamp
+			timestamp_tdata		:	IN	STD_LOGIC_VECTOR(BIT_FID + BIT_COARSE + BIT_RESOLUTION-1 DOWNTO 0); 	    -- Timestamp dFID + COARSE + RESOLUTION
+			-----------------------------------------------
+
+			--------------- BeltBus Output ----------------
+		    beltbus_tvalid	   :	OUT	STD_LOGIC;															    	-- Valid Belt Bus
+			beltbus_tdata	   :	OUT	STD_LOGIC_VECTOR(BIT_FID + BIT_COARSE + BIT_RESOLUTION-1 DOWNTO 0)	    	-- Belt Bus
+			-----------------------------------------------
+
+		);
+
+	END COMPONENT;
+
+
+begin
+
+	------------------ Components instantiation --------------------
+
+
+	---------- OverflowCounter -----------
+	Inst_OverflowCounter : OverflowCounter
+		GENERIC MAP (
+
+			---------- Calibrated Timestamp Dimension ----
+			BIT_FID			=>	BIT_FID,
+			BIT_COARSE   	=>	BIT_COARSE,
+
+			BIT_RESOLUTION	=>	BIT_RESOLUTION
+			--------------------
+
+		)
+		PORT MAP(
+
+			------ Reset ------
+			reset	=> reset,
+			-------------------
+
+			------ Clocks ------
+			clk		=> clk,
+			--------------------
+
+			--------------- Timestamp Input ---------------
+			timestamp_tvalid	=> s00_timestamp_tvalid,
+			timestamp_tdata		=> s00_timestamp_tdata(BIT_FID + BIT_COARSE + BIT_RESOLUTION-1 DOWNTO 0),
+			-----------------------------------------------
+
+			--------------- BeltBus Output ----------------
+			beltbus_tvalid	    => m00_beltbus_tvalid,
+			beltbus_tdata		=> m00_beltbus_tdata(BIT_FID + BIT_COARSE + BIT_RESOLUTION-1 DOWNTO 0)
+			-----------------------------------------------
+		);
+	---------------------------------
+
+
+	------------------------------------------------------------------
+
+
+
+end Behavioral;
--- README.md
+++ README.md
@@ -9,6 +9,7 @@ The timestamps in output of the OverflowCounter is an AXI4 Stream interface.
 We can find in *"OverflowCounter/OverflowCounter.scrs/sources_1"* the following module directory:
 
  - **AXI4Stream_OverflowCounter**: Wrapper used for rename the input and output interfaces with AXI4-Stream, input as slave and output as a master.
+ - **AXI4Stream_OverflowCounterWrapper**: Wrapper used for rename the input and output interfaces with presudo AXI4-Stream, input as slave and output as a master.
  - **OverflowCounter**: Count the number of Overflow in the input data(*s00_timestamp_tdata*).
 
 
@@ -36,6 +37,30 @@ Wrapping of OverflowCounter for AXI4-Stream interface.
      - **m00_beltbus_tdata**: Output data with the proper FID padded to upper byte dimension *STD_LOGIC_VECTOR((((BIT_FID + BIT_COARSE + BIT_RESOLUTION-1)/8+1)* * *8)-1 DOWNTO 0)* type, used only *STD_LOGIC_VECTOR(BIT_FID + BIT_COARSE + BIT_RESOLUTION-1 downto 0)* type.
 
 The module instantiates the *OverflowCounter*, set to '0' the MSBs of the output data (*m00_beltbus_tdata(m00_beltbus_tdata'LENGTH-1 downto BIT_FID + BIT_COARSE + BIT_RESOLUTION)*) and rename the input and output interfaces with AXI4-Stream, input as slave and output as master.
+
+
+## AXI4Stream_OverflowCounterWrapper
+Wrapping of OverflowCounter for AXI4-Stream interface.
+
+### Generic
+
+ - **BIT_FID**: Bit Dimension of the Fid part of the Timestamp, *NATURAL* type. If *BIT_FID = 0* the belt bus is removed and it is a standard axi4 stream.
+ - **BIT_COARSE**: Bit Dimension of the Coarse part of the Timestamp, *NATURAL* type *RANGE 0 TO 32*.
+ - **BIT_RESOLUTION**: Bit Dimension of the Fine part of the Timestamp, *POSITIVE* type *RANGE 1 TO 32*.
+
+### Port
+
+ - **reset**: Asynchronous system reset active high (if '1' goto reset state).
+
+ - **clk**: System clock.
+
+ - **s00_timestamp**: AXI4 Stream Slave (Input) interface, uncalibrated data coming from the previous module().
+     - **s00_timestamp_tvalid**: Valid of the input data, *STD_LOGIC* type.
+     - **s00_timestamp_tdata**: Input data (timestamp) padded to upper byte dimension, *STD_LOGIC_VECTOR(BIT_FID + BIT_COARSE + BIT_RESOLUTION-1 DOWNTO 0)* type, used only *STD_LOGIC_VECTOR(BIT_FID + BIT_COARSE + BIT_RESOLUTION-1 downto 0)* type.
+
+ - **m00_beltbus**: AXI4 Stream master (Output) interface, containing the value of the input data (*s00_timestamp_tdata*) or the number of Overflow depending on the value of the FID.
+     - **m00_beltbus_tvalid**: Valid of the output data, *STD_LOGIC* type.
+     - **m00_beltbus_tdata**: Output data with the proper FID padded to upper byte dimension *STD_LOGIC_VECTOR(BIT_FID + BIT_COARSE + BIT_RESOLUTION-1 DOWNTO 0)* type, used only *STD_LOGIC_VECTOR(BIT_FID + BIT_COARSE + BIT_RESOLUTION-1 downto 0)* type.
 
 
 #OverflowCounter
