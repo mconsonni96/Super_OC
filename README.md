0158ab4c8286fdce7f178bfac4fd87e1213cc742
Mattia Consonni <mattia2.consonni@mail.polimi.it>
2022-02-14 16:30:16 +0100

    documentazione aggiornata

--- README.md
+++ README.md
@@ -5,10 +5,12 @@ The output depends on the value of the most significant bits(*FID*) of the input
 The timestamps in output of the OverflowCounter is an AXI4 Stream interface.
 
 If *BIT_FID = 0* the Belt Bus is removed and the output is a standard Axi4 Stream. In this case the module is transparent and the input is transferred unchanged to the output (*beltbus_tvalid <= timestamp_tvalid*, *beltbus_tdata <= timestamp_tdata*). Moreover the Counter of Overflow(*CoarseOverflow_cnt*) is not enabled.
-If *BIT_FID /= 0* and *timestamp_tvalid = '1'*, the output corresponds exactly to the input (*beltbus_tdata <= timestamp_tdata*) if you have a measure (*fid = '1'*), otherwise if you have an Overflow (*fid = 0*) the *CoarseOverflow_cnt* is increased by one and the output contains the *fid* & *CoarseOverflow_cnt*.
+If *BIT_FID /= 0* and *timestamp_tvalid = '1'*, the output has the following structure: if you have a measure (*fid_in = '1'*), the Fine part remains unchanged, the BIT_COARSE_IN bits of the Coarse part remains equal to the input ones, while the BIT_COARSE_OUT - BIT_COARSE_IN bits of the Coarse part are equal to an AuxiliaryCounter, which counts the number of input data which are overflows (*fid_in = 0*).
+When AuxiliaryCounter arrives at all '1's and another data with *fid_in = 0* arrives, it restarts from zero and i have as output data the *CoarseOverflow_cnt*, which is increased by one; so, the output contains the *fid_overflow* (00) & *CoarseOverflow_cnt*.
+If *BIT_COARSE_CEC = BIT_COARSE*, the ip-core behaves as the older version, where we have in ouput *CoarseOverflow_cnt* which increases by one every time you have in input *fid_in = '0'*, or the measure if you have in input *fid_in = '1'*.
 
 
-![InputOutput Image](doc/img/InputOutput.svg)
+![InputOutput Image](doc/img/wave_super.png)
 
 
 # IP-Core
@@ -19,7 +21,8 @@ OverflowCounter for AXI4-Stream interface IP-Core.
 ## Generic
 
  - **BIT_FID**: Bit Dimension of the Fid part of the Timestamp, *NATURAL* type. If *BIT_FID = 0* the belt bus is removed and it is a standard axi4 stream.
- - **BIT_COARSE**: Bit Dimension of the Coarse part of the Timestamp, *NATURAL* type *RANGE 0 TO 32*.
+ - **BIT_COARSE_CEC**: Bit Dimension of the Coarse part of the input Timestamp, *NATURAL* type *RANGE 0 TO 32*.
+ - **BIT_COARSE**: Bit Dimension of the Coarse part of the output Timestamp, *NATURAL* type *RANGE 0 TO 128*.
  - **BIT_RESOLUTION**: Bit Dimension of the Fine part of the Timestamp, *POSITIVE* type *RANGE 1 TO 32*.
 
  ![Generic Image](doc/img/OverflowCounter_Generic.png)
@@ -32,7 +35,7 @@ OverflowCounter for AXI4-Stream interface IP-Core.
 
  - **s00_timestamp**: AXI4 Stream Slave (Input) interface, uncalibrated data coming from the previous module().
      - **s00_timestamp_tvalid**: Valid of the input data, *STD_LOGIC* type.
-     - **s00_timestamp_tdata**: Input data (timestamp) padded to upper byte dimension, *STD_LOGIC_VECTOR((((BIT_FID + BIT_COARSE + BIT_RESOLUTION-1)/8+1)* * *8)-1 DOWNTO 0)* type, used only *STD_LOGIC_VECTOR(BIT_FID + BIT_COARSE + BIT_RESOLUTION-1 downto 0)* type.
+     - **s00_timestamp_tdata**: Input data (timestamp) padded to upper byte dimension, *STD_LOGIC_VECTOR((((BIT_FID + BIT_COARSE_CEC + BIT_RESOLUTION-1)/8+1)* * *8)-1 DOWNTO 0)* type, used only *STD_LOGIC_VECTOR(BIT_FID + BIT_COARSE_CEC + BIT_RESOLUTION-1 downto 0)* type.
 
  - **m00_beltbus**: AXI4 Stream master (Output) interface, containing the value of the input data (*s00_timestamp_tdata*) or the number of Overflow depending on the value of the FID.
      - **m00_beltbus_tvalid**: Valid of the output data, *STD_LOGIC* type.
@@ -55,4 +58,4 @@ We can find in  *src/* the following module directory:
  - **tb_AXI4Stream_OverflowCounter_behav**: Waveform of *tb_AXI4Stream_OverflowCounter*.
 
 
-![wave Image](doc/img/wave.png)
+![wave Image](doc/img/wave_super.png)
--- hdl/OverflowCounter.vhd
+++ hdl/OverflowCounter.vhd
@@ -75,8 +75,8 @@ entity OverflowCounter is
 
 		---------- Calibrated Timestamp Dimension ----
 	    BIT_FID				    :	NATURAL							:=	0;			        --! Bit Dimension of the Fid part of the Timestamp. If BIT_FID = 0 the belt bus is removed and it is a standard axi4 stream
-		BIT_COARSE_IN			:	NATURAL		RANGE	0   TO	32	:=	8;
-		BIT_COARSE_OUT          :   NATURAL     RANGE   0   TO  128 :=  32;					--! Bit Dimension of the Coarse part of the Timestamp. If 0 not Coarse counter is considered only Fine
+		BIT_COARSE_IN			:	NATURAL		RANGE	0   TO	32	:=	8;                  --! Bit Dimension of the input data Coarse Part of the Timestamp. It corresponds to the one in the CoarseExtensionCore.
+		BIT_COARSE_OUT          :   NATURAL     RANGE   0   TO  128 :=  32;					--! Bit Dimension of the output data Coarse Part of the Timestamp. It corresponds to the Coarse part length in all the following modules.
 		BIT_RESOLUTION          :	POSITIVE	RANGE	1	TO	32	:=	16					--! Bit Dimension of the Fine part of the Timestamp, number of Bits of the Calibrated_TDL
 		----------------------------------------------
 	);
@@ -95,7 +95,7 @@ entity OverflowCounter is
 
 		--------------- Timestamp Input ---------------
 		timestamp_tvalid	:	IN	STD_LOGIC;															    	--! Valid Timestamp
-		timestamp_tdata		:	IN	STD_LOGIC_VECTOR(BIT_FID + BIT_COARSE_IN + BIT_RESOLUTION-1 DOWNTO 0); 	    --! Timestamp FID + COARSE + RESOLUTION
+		timestamp_tdata		:	IN	STD_LOGIC_VECTOR(BIT_FID + BIT_COARSE_IN + BIT_RESOLUTION-1 DOWNTO 0); 	    --! Timestamp FID + COARSE_IN + RESOLUTION
 		-----------------------------------------------
 
 		-------------- Calibrated Input ---------------
@@ -115,11 +115,12 @@ end OverflowCounter;
 --! \brief If *BIT_FID = 0* the Belt Bus is removed and the output is a standard Axi4 Stream.
 --! In this case the module is trasparent and the input is transferred unchanged to the output (*beltbus_tvalid <= timestamp_tvalid*, *beltbus_tdata <= timestamp_tdata*).
 --! \details Moreover the Counter of Overflow(*CoarseOverflow_cnt*) is not enabled.
---! If *BIT_FID /= 0* and *timestamp_tvalid = '1'*, the output corresponds exactly
---! to the input (*beltbus_tdata <= timestamp_tdata*) if you have a measure (*fid = '1'*),
---! otherwise if you have an Overflow (*fid = 0*) the *CoarseOverflow_cnt* is increased by one and the output contains the *fid* & *CoarseOverflow_cnt*.
+--! If *BIT_FID /= 0* and *timestamp_tvalid = '1'*, the output has the following structure:
+--! if you have a measure (*fid_in = '1'*), the Fine part remains unchanged, the BIT_COARSE_IN bits of the Coarse part remains equal to the input ones,
+--! while the BIT_COARSE_OUT - BIT_COARSE_IN bits of the Coarse part are equal to an AuxiliaryCounter, which counts the number of input data which are overflows (*fid_in = 0*).
+--! When AuxiliaryCounter arrives at all '1's and another data with *fid_in = 0* arrives, it restarts from zero and i have as output data the *CoarseOverflow_cnt*, which is increased by one; so, the output contains the *fid_overflow* (00) & *CoarseOverflow_cnt*.
 --! In the following figure we can see the concept just explained
---! \image html InputOutput.svg
+--! \image html wave_super.png
 --------------------------------------------------------------------------------
 
 architecture Behavioral of OverflowCounter is
@@ -127,17 +128,17 @@ architecture Behavioral of OverflowCounter is
 	------------------------- CONSTANTS DECLARATION ----------------------------
 
 	----- Coarse Counter OverFlow Dimension -----
-	constant	BIT_OVERFLOW_CNT	:	POSITIVE	:=	BIT_COARSE_OUT + BIT_RESOLUTION;							--! Coarse Counter OverFlow Dimension
+	constant	BIT_OVERFLOW_CNT	:	POSITIVE	:=	BIT_COARSE_OUT + BIT_RESOLUTION;							--! Output Coarse Counter OverFlow Dimension
 	----------------------------------------------
-    constant    BIT_COARSE_DIFFERENCE : NATURAL     :=  BIT_COARSE_OUT - BIT_COARSE_IN;
+    constant    BIT_COARSE_DIFFERENCE : NATURAL     :=  BIT_COARSE_OUT - BIT_COARSE_IN;                             --! Difference of the Coarse part Length in output minus the one in input
 
-	constant    MAX_AUX_COUNT       :   INTEGER    := 2**(BIT_COARSE_DIFFERENCE);
+	constant    MAX_AUX_COUNT       :   INTEGER    := 2**(BIT_COARSE_DIFFERENCE);                                   --! Maximum value of the AuxiliaryCounter part. It restarts from zero.
 
 	----------- FID of the BeltBus --------------
-	constant	FID_OVERFLOW		:	STD_LOGIC_VECTOR(BIT_FID-1 downto 0)	:=	(Others => '0');			--! 0 means overflow
+	constant	FID_OVERFLOW		:	STD_LOGIC_VECTOR(BIT_FID-1 downto 0)	:=	(Others => '0');			    --! 0 means overflow
 	---------------------------------------------
 	----------------------------------------------------------------------------
-    constant    ZERO_PADDING        :   STD_LOGIC_VECTOR(BIT_COARSE_DIFFERENCE -1 downto 0) := (Others => '0');     --! vector for zero padding in coarse part of the output data (measure)
+    constant    ZERO_PADDING        :   STD_LOGIC_VECTOR(BIT_COARSE_DIFFERENCE -1 downto 0) := (Others => '0');     --! vector for zero padding in coarse part of the output data (measure) in the case BIT_FID = 0
 
 	----------------------------- ALIAS DECLARATIONS ---------------------------
 	----------- FID of the BeltBus --------------
@@ -149,9 +150,8 @@ architecture Behavioral of OverflowCounter is
 
 	-------------------------- SIGNALS DECLARATION -----------------------------
 	---------- Coarse Counter OverFlow  ----------
-	signal  CoarseOverflow_cnt_out  :   UNSIGNED(BIT_OVERFLOW_CNT-1 downto 0);
-	signal  AuxiliaryCounter        :   UNSIGNED(BIT_COARSE_DIFFERENCE -1 downto 0)  := (Others => '0');										--! Overflow Counter
-    signal  CoarseVector            :   UNSIGNED(BIT_COARSE_OUT -1 downto 0)         := (Others => '0');
+	signal  CoarseOverflow_cnt_out  :   UNSIGNED(BIT_OVERFLOW_CNT-1 downto 0);                                --! Overflow Counter
+	signal  AuxiliaryCounter        :   UNSIGNED(BIT_COARSE_DIFFERENCE -1 downto 0)  := (Others => '0');	  --! AuxiliaryCounter needed to count the number of input data with *fid_in = 0*
     ----------------------------------------------
 	----------------------------------------------------------------------------
 
@@ -201,7 +201,7 @@ begin
 					    elsif BIT_COARSE_DIFFERENCE = 0 then
 						      beltbus_tvalid <= '1';
 						      CoarseOverflow_cnt_out <= CoarseOverflow_cnt_out + 1;
-						      beltbus_tdata	<=	FID_OVERFLOW & std_logic_vector(CoarseOverflow_cnt_out +1); 
+						      beltbus_tdata	<=	FID_OVERFLOW & std_logic_vector(CoarseOverflow_cnt_out +1);
                         end if;
 
 					end if;
